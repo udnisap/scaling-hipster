0d50e177bba7d0031c35702973622a3e361cdbd7
Sadari Jayawardena <sjayawardena603@gmail.com>
2014-01-21 19:47:24 +0530

    add status function

--- master/script.sh
+++ master/script.sh
@@ -5,7 +5,7 @@ OPTIONS="Deploy Run"
 
 # run (uri, parameters) 
 function run {
-	URI=$1
+	URI=$1"?method=run"
 	params=$2
 	if [ -z "$params" ]; then 
 		echo Running $URI
@@ -20,6 +20,18 @@ function run {
 
 function deploy {
 	echo "deploy script"	
+}
+
+#status URI
+function status {
+	URI=$1"?method=status"
+	if [ -z "$URI" ]; then 
+		echo Enter host name
+		exit
+	else
+		response=$(curl -s $URI 2>&1);
+		#echo $response
+	fi
 }
 
 function init {
