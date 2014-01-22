08a546caf3607485f2874f256a5c4918b8ad5f26
isuru <irjanith@gmail.com>
2014-01-22 16:36:15 +0530

    remote create adn delete fucntions

--- master/script.sh
+++ master/script.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 OPTIONS="Deploy Run"
-
-
+openShiftGitRepo="git://github.com/isururanawaka/Openshift.git"
+herokuGitRepo="git://github.com/heroku/ruby-sample.git"
+herokuAppFolder="ruby-sample"
+openShiftNewAppURL=""
+OpenShiftNewAppGitRepo=""
+herokuNewAppURL=""
+herokuNewGitRepo=""
 
 # run (uri, parameters) 
 function run {
@@ -16,6 +21,99 @@ function run {
 	fi
 
 }
+
+#create(type,name)
+function create {
+	typed=$1
+	name=$2
+	if [ -z "$typed" ]; then 
+		echo please give Paas e.g heroku, openshift
+		
+	elif [ -z "$name" ]; then
+		echo please give application name
+        
+        elif [ "$typed" = "openshift" ]; then
+                 rhc setup
+		 rhc app create -a $name -t php-5.3 
+                 cd $name
+                 git remote add upstream -m master $openShiftGitRepo
+                 git pull -s recursive -X theirs upstream master
+                 git push
+                 cd ..
+                 response=$(rhc app-show $name)
+                 javac AppInfoParser.java
+                 openShiftNewAppURL=$(java AppInfoParser "$response" openshift appurl)
+                 OpenShiftNewAppGitRepo=$(java AppInfoParser "$response" openshift gitrepo)
+                 echo URL of App: $openShiftNewAppURL
+                 echo Git Repo :$OpenShiftNewAppGitRepo
+        elif [ "$typed" = "heroku" ]; then
+ 
+                 git clone $herokuGitRepo
+                 cd $herokuAppFolder
+                 heroku create $name
+                 git push heroku master 
+                 responseheroku=$(heroku apps:info)
+                 cd ..
+                 javac AppInfoParser.java
+                 herokuNewAppURL=$(java AppInfoParser "$responseheroku" heroku appurl)
+                 herokuNewGitRepo=$(java AppInfoParser "$responseheroku" heroku gitrepo)
+                 echo URL of App: $herokuNewAppURL
+                 echo Git Repo :$herokuNewGitRepo
+                # heroku open
+       
+        else
+            echo cannot create  
+             getAppData
+        fi
+}
+
+#getAppData(appname,apptype,type)
+function getAppData {
+  response=$(rhc app-show testingapp)
+  javac AppInfoParser.java
+  java AppInfoParser "$response" openshift gitrepo	
+ # echo $response			
+}
+
+
+
+# delete (type, name)
+function delete {
+    delapptype=$1
+    delapp=$2
+    if [ -z "$delapptype" ]; then 
+		echo please give paas 
+     elif [ -z "$delapp" ]; then
+                echo please give app name
+     elif [ "$delapptype" = "openshift" ]; then
+          
+          rhc app delete $delapp
+          rm -rf $delapp
+          echo successfully deleted $delapp
+      elif [ "$delapptype" = "heroku" ]; then
+          
+          heroku apps:delete $delapp --confirm $delapp
+          rm -rf $herokuAppFolder
+          echo successfully deleted $delapp
+     else
+          echo cannot delete
+     fi
+				
+}
+
+
+# read Password
+function readpassword {
+    echo $(sed -n 's/.*password *= *\([^ ]*.*\)/\1/p' < parameters.ini)
+}
+
+# read UserName
+function readusername {
+    echo $(sed -n 's/.*username *= *\([^ ]*.*\)/\1/p' < parameters.ini)
+}
+
+
+
 
 
 function deploy {
@@ -39,15 +137,24 @@ function init {
 }
 
 
-
 if [ -z "$1" ]; then 
       echo Enter Either run or deploy
       exit
 fi
 
-if [ "$1" = "run" ]; then
-	run $2 $3		
-fi
 
+
+if [ "$1" = "run" ]; then
+        run $2 $3
+else
+  if [ "$1" = "delete" ]; then
+	delete $2 $3
+  else 
+   if [ "$1" = "create" ]; then
+        create $2 $3
+    fi
+  fi
+ fi
+ 
 
 
